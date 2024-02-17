379a7598fb592318b298959317d08ccdc92e9ef2
Mike Pattyn <mike.pattyn1@gmail.com>
2024-02-17 12:00:42 +0100

    remove appstore and console log

--- apps/frontend/angular/src/app/interceptors/auth.interceptor.ts
+++ apps/frontend/angular/src/app/interceptors/auth.interceptor.ts
@@ -2,19 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UserProfileStateService } from '../services/user-profile-state.service';
-import { AppStore } from '../app.store';
 
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
-  
-  constructor(private stateService: UserProfileStateService, private appStore: AppStore) {
 
-  }
+  constructor(private stateService: UserProfileStateService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('caught request', req);
-    
-    // Retrieve the user token from the AuthService
     const userToken = this.stateService.idToken;
     
     // Clone the HTTP request and add the authorization header with the token
