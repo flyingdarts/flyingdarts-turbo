782d40dbbc51d52ab706c9a2ff945a27e4ce5108
Mike Pattyn <mike.pattyn1@gmail.com>
2025-08-09 12:01:50 +0200

    Configure environments properly

--- apps/frontend/angular/fd-app/src/environments/environment.development.ts
+++ apps/frontend/angular/fd-app/src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   authressAppId: 'app_2YKyhM6M31XVtuCeuDsSJ2',
   authressLoginUrl: 'https://authress.flyingdarts.net/',
-  webSocketUrl: 'wss://r051b8g6e7.execute-api.eu-west-1.amazonaws.com/Development',
-  usersApi: 'https://hl9c7h9z05.execute-api.eu-west-1.amazonaws.com/prod',
-  friendsApi: 'https://hl9c7h9z05.execute-api.eu-west-1.amazonaws.com/prod/friends',
+  webSocketUrl: 'wss://6pzat9979d.execute-api.eu-west-1.amazonaws.com/Development',
+  usersApi: 'https://hye8b8qfx8.execute-api.eu-west-1.amazonaws.com/Development',
+  friendsApi: 'https://hye8b8qfx8.execute-api.eu-west-1.amazonaws.com/Development/friends',
 };
---
+++ apps/frontend/angular/fd-app/src/environments/environment.staging.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  authressAppId: 'app_2YKyhM6M31XVtuCeuDsSJ2',
+  authressLoginUrl: 'https://authress.flyingdarts.net/',
+  webSocketUrl: 'wss://blzcncplv6.execute-api.eu-west-1.amazonaws.com/Staging',
+  usersApi: 'https://hm6wrdp5ag.execute-api.eu-west-1.amazonaws.com/Staging',
+  friendsApi: 'https://hm6wrdp5ag.execute-api.eu-west-1.amazonaws.com/Staging/friends',
+};
