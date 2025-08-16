c205415e13488c29173754699663d60780abf33d
Mike Pattyn <mike.pattyn1@gmail.com>
2025-08-16 09:05:19 +0200

    shorter name

--- apps/frontend/angular/fd-app/src/app/features/game/components/stats/presentation/game-stats.component.ts
+++ apps/frontend/angular/fd-app/src/app/features/game/components/stats/presentation/game-stats.component.ts
@@ -27,4 +27,18 @@ export class GameStatsComponent {
     }
     return darts.join(' | ');
   }
+
+  getPlayerName(player: GameStateStates.PlayerState): string {
+    if (player.name.substring(0, 2) === 'sc') {
+      return 'SC Player';
+    }
+    return player.name;
+  }
+
+  getOpponentName(opponent: GameStateStates.PlayerState): string {
+    if (opponent.name.substring(0, 2) === 'sc') {
+      return 'SC Opponent';
+    }
+    return opponent.name;
+  }
 }
